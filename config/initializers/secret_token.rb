0f9f7cf67853f45ea769d3aff31313a4c1705507
Michael <Michael.Eberspaecher@mhm-hr.com>
2013-07-01 16:26:00 +0200

    Bootstrap eingebunden

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveyTool326::Application.config.secret_token = '56c016b687f14c437ea2ebdae1db143737538b7856cca921f4404673b8bbf07022c38c21953e16d7257e22a20e13b4a5028b20add7904e409f22e3922c66c879'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SurveyTool326::Application.config.session_store :cookie_store, key: '_survey_tool_326_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SurveyTool326::Application.config.session_store :active_record_store
